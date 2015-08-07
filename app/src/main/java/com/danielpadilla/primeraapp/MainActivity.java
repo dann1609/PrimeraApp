bd9de857341d5bb0585fd9704f217590947e3d79
Daniel Padilla <danperzipa@hotmail.com>
2015-08-07 05:55:13 -0500

    Primer commit

---
+++ app/src/main/java/com/danielpadilla/primeraapp/MainActivity.java
@@ -0,0 +1,27 @@
+package com.danielpadilla.primeraapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+    public void Metodo1(View view) {
+        Button b = (Button)view;
+        String buttonText = b.getText().toString();
+        Toast.makeText(this,buttonText,Toast.LENGTH_SHORT).show();
+    }
+}
